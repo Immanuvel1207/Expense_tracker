6e6871883405801d806cfc1ef401c89d86cd4558
Immanuvel <140709775+Immanuvel1207@users.noreply.github.com>
2024-10-03 23:51:06 +0530

    Update Header.js

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -6,7 +6,7 @@ import './Header.css';
 const Header = () => {
   return (
     <header className="header">
-      <div className="logo">Expense Tracker</div>
+      <div className="logo">Expense Tracker by Immanuvel</div>
       <nav>
         <ul>
           <li><Link to="/login">Login</Link></li>
